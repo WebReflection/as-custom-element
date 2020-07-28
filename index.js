f878a51e56585754edb1f2fc6ba80c13af6cc3ec
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-07-28 17:54:49 +0200

    using as-ce instead

--- index.js
+++ index.js
@@ -15,100 +15,101 @@ self.asCustomElement = (function (exports) {
     callback(this, observer);
   }
 
-  var wm = new WeakMap();
-  var observer$1 = set.observer;
+  var asCE = (function (selectors) {
+    var wm = new WeakMap();
 
-  var attributeChanged = function attributeChanged(records) {
-    var _loop = function _loop(i, length) {
-      var _records$i = records[i],
-          target = _records$i.target,
-          attributeName = _records$i.attributeName,
-          oldValue = _records$i.oldValue;
-      var newValue = target.getAttribute(attributeName);
-      wm.get(target).a[attributeName].forEach(function (attributeChangedCallback) {
-        attributeChangedCallback.call(target, attributeName, oldValue, newValue);
-      });
-    };
+    var attributeChanged = function attributeChanged(records) {
+      var _loop = function _loop(i, length) {
+        var _records$i = records[i],
+            target = _records$i.target,
+            attributeName = _records$i.attributeName,
+            oldValue = _records$i.oldValue;
+        var newValue = target.getAttribute(attributeName);
+        wm.get(target).a[attributeName].forEach(function (attributeChangedCallback) {
+          attributeChangedCallback.call(target, attributeName, oldValue, newValue);
+        });
+      };
 
-    for (var i = 0, length = records.length; i < length; i++) {
-      _loop(i);
-    }
-  };
-
-  var invoke$1 = function invoke(nodes, key, parsed, noCheck) {
-    for (var i = 0, length = nodes.length; i < length; i++) {
-      var target = nodes[i];
-
-      if (!parsed.has(target) && (noCheck || 'querySelectorAll' in target)) {
-        parsed.add(target);
-        if (wm.has(target)) wm.get(target)[key].forEach(call, target);
-        invoke(target.querySelectorAll('*'), key, parsed, true);
+      for (var i = 0, length = records.length; i < length; i++) {
+        _loop(i);
       }
-    }
-  };
-
-  var mainLoop = function mainLoop(records) {
-    for (var i = 0, length = records.length; i < length; i++) {
-      var _records$i2 = records[i],
-          addedNodes = _records$i2.addedNodes,
-          removedNodes = _records$i2.removedNodes;
-      invoke$1(addedNodes, 'c', new Set(), false);
-      attributeChanged(sao.takeRecords());
-      invoke$1(removedNodes, 'd', new Set(), false);
-    }
-  };
-
-  var sao = new MutationObserver(attributeChanged);
-
-  var set$1 = function set(target) {
-    var sets = {
-      a: {},
-      c: new Set(),
-      d: new Set()
     };
-    wm.set(target, sets);
-    return sets;
-  };
 
-  set.add(mainLoop);
-  var index = (function (target, _ref) {
-    var connectedCallback = _ref.connectedCallback,
-        disconnectedCallback = _ref.disconnectedCallback,
-        observedAttributes = _ref.observedAttributes,
-        attributeChangedCallback = _ref.attributeChangedCallback;
-    mainLoop(observer$1.takeRecords());
+    var invoke = function invoke(nodes, key, parsed, noCheck) {
+      for (var i = 0, length = nodes.length; i < length; i++) {
+        var target = nodes[i];
 
-    var _ref2 = wm.get(target) || set$1(target),
-        a = _ref2.a,
-        c = _ref2.c,
-        d = _ref2.d;
+        if (!parsed.has(target) && (noCheck || 'querySelectorAll' in target)) {
+          parsed.add(target);
+          if (wm.has(target)) wm.get(target)[key].forEach(call, target);
+          invoke(target.querySelectorAll(selectors), key, parsed, true);
+        }
+      }
+    };
 
-    if (observedAttributes) {
-      sao.observe(target, {
-        attributes: true,
-        attributeOldValue: true,
-        attributeFilter: observedAttributes
-      });
-      observedAttributes.forEach(function (attributeName) {
-        (a[attributeName] || (a[attributeName] = new Set())).add(attributeChangedCallback);
-        if (target.hasAttribute(attributeName)) attributeChangedCallback.call(target, attributeName, null, target.getAttribute(attributeName));
-      });
-    }
+    var mainLoop = function mainLoop(records) {
+      for (var i = 0, length = records.length; i < length; i++) {
+        var _records$i2 = records[i],
+            addedNodes = _records$i2.addedNodes,
+            removedNodes = _records$i2.removedNodes;
+        invoke(addedNodes, 'c', new Set(), false);
+        attributeChanged(sao.takeRecords());
+        invoke(removedNodes, 'd', new Set(), false);
+      }
+    };
 
-    if (disconnectedCallback) d.add(disconnectedCallback);
+    var set$1 = function set(target) {
+      var sets = {
+        a: {},
+        c: new Set(),
+        d: new Set()
+      };
+      wm.set(target, sets);
+      return sets;
+    };
 
-    if (connectedCallback) {
-      c.add(connectedCallback); // if (target.isConnected) // No IE11/Edge support
+    var sao = new MutationObserver(attributeChanged);
+    set.add(mainLoop);
+    return function (target, _ref) {
+      var connectedCallback = _ref.connectedCallback,
+          disconnectedCallback = _ref.disconnectedCallback,
+          observedAttributes = _ref.observedAttributes,
+          attributeChangedCallback = _ref.attributeChangedCallback;
+      mainLoop(set.observer.takeRecords());
 
-      if (!(target.ownerDocument.compareDocumentPosition(target) & target.DOCUMENT_POSITION_DISCONNECTED)) connectedCallback.call(target);
-    }
+      var _ref2 = wm.get(target) || set$1(target),
+          a = _ref2.a,
+          c = _ref2.c,
+          d = _ref2.d;
 
-    return target;
+      if (observedAttributes) {
+        sao.observe(target, {
+          attributes: true,
+          attributeOldValue: true,
+          attributeFilter: observedAttributes
+        });
+        observedAttributes.forEach(function (attributeName) {
+          (a[attributeName] || (a[attributeName] = new Set())).add(attributeChangedCallback);
+          if (target.hasAttribute(attributeName)) attributeChangedCallback.call(target, attributeName, null, target.getAttribute(attributeName));
+        });
+      }
+
+      if (disconnectedCallback) d.add(disconnectedCallback);
+
+      if (connectedCallback) {
+        c.add(connectedCallback);
+        if (!(target.ownerDocument.compareDocumentPosition(target) & target.DOCUMENT_POSITION_DISCONNECTED)) connectedCallback.call(target);
+      }
+
+      return target;
+    };
   });
 
   function call(back) {
     back.call(this);
   }
+
+  var index = asCE(['*']);
 
   exports.default = index;
 
